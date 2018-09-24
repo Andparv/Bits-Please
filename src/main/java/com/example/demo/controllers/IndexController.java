b97fdf5d986e6e6ffd8837a87b659ea4f0d40133
brigitta <rebanebrigitta@gmail.com>
2018-09-24 16:56:21 +0300

    Midagi laeb

---
+++ src/main/java/com/example/demo/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping("/")
+    public String welcome(Model model) {
+        //model.addAttribute("text", "This is text");
+        return "index";
+    }
+}
