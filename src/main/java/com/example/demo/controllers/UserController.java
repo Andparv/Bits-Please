361494021f795e390a97681e65e72045bbcb01e8
brigitta <rebanebrigitta@gmail.com>
2018-11-05 14:40:45 +0200

    registrating

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -2,26 +2,24 @@ package com.example.demo.controllers;
 
 import com.example.demo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.security.Principal;
 
-@RestController
+@Controller
 public class UserController {
 
     @Autowired
     UserService userService;
 
     @PostMapping("/register")
-    @ResponseBody
-    public ResponseEntity registerUser(@ModelAttribute("email") String email, Principal principal){
+    public String registerUser(@ModelAttribute("email") String email, Principal principal){
         if (userService.getUser(email)!=null){
-            return new ResponseEntity(HttpStatus.CONFLICT);
+            return "HomePage";
         }
         userService.addUser(email, principal);
-        return new ResponseEntity(HttpStatus.CREATED);
+        return "HomePage";
     }
-
 }
