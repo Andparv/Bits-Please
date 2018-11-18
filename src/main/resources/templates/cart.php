3e9b606ad9c1f49fed4693de43faf0ba02a308b2
andparv <andparv@gmail.com>
2018-11-18 22:24:20 +0200

    cart change

---
+++ src/main/resources/templates/cart.php
@@ -0,0 +1,11 @@
+<?php
+header("Content-type: text/xml");
+echo "<?xml version='1.0' encoding='UTF-8'?>";
+echo "<CART>";
+echo "<PRODUCT>";
+echo "<NAME>Placeholder</NAME>";
+echo "<PRICE>99.99$</PRICE>";
+echo "<QUANTITY>1</QUANTITY>";
+echo "</PRODUCT>";
+echo "</CART>";
+?>
