68e4ba37b9d99d1f93211d9df104bfd4457970d1
Alfred Kraft <alfaktk2@gmail.com>
2018-11-18 22:43:13 +0200

    Külastajate andmete salvestamine test1

--- src/main/resources/static/HomePage.js
+++
@@ -1,7 +0,0 @@
-// JavaScript source code
-$('.search-link').on('click', function () {
-    $(this).hide()
-    $('.search-bar').show()
-});
-
-
---
+++ src/main/resources/static/js/külastajaAndmed.php
@@ -0,0 +1,23 @@
+<?php
+
+$conn = new mysqli('eu-cdbr-west-02.cleardb.net', 'b864d9547483d4', '900c8909', 'heroku_6e9e2e43ea89231'); // Create connection
+if ($conn->connect_error) {     // Check connection
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$date = mysqli_real_escape_string($conn, $_POST['date']);
+$IP = mysqli_real_escape_string($conn, $_POST['IP']);
+$Platform = mysqli_real_escape_string($conn, $_POST['Platform']);
+
+if (strlen($times) > 200000) {  $times = "";    }
+
+$sql = "INSERT INTO visitors
+VALUES ('$date', '$IP', '$Platform')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Page saved!";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+$conn->close();
+?>
