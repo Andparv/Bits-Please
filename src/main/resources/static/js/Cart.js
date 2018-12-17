3b46a5bfe955daa2503d520e7c0ace7b1f35a835
Alfred Kraft <alfaktk2@gmail.com>
2018-12-17 04:33:19 +0200

    Ausalt lähen magama nüüd

--- src/main/resources/static/js/Cart.js
+++ src/main/resources/static/js/Cart.js
@@ -33,13 +33,15 @@ function addToCartClicked(event) {
         var pricetext = shopItem.getElementsByClassName('price')[0].innerText.replace('Price: ', '')
         pricetext.replace('€', '')
         var price = parseFloat(pricetext)
+        var keel = "Inglise"
     }
     else {
         var pricetext = shopItem.getElementsByClassName('price')[0].innerText.replace('Hind: ', '')
         pricetext.replace('€', '')
         var price = parseFloat(pricetext)
+        var keel = "Eesti"
     }
-    addItemToCart(title, price)
+    addItemToCart(title, price, keel)
 
 
     console.log(title, price)
@@ -56,7 +58,7 @@ function quantityChanged(event) {
     }
     updateCartTotal()
 }
-function addItemToCart(title, price) {
+function addItemToCart(title, price, keel) {
     var cartRow = document.createElement('div')
     cartRow.classList.add('cart-row')
     var cartItems = document.getElementsByClassName('cart-items')[0]
@@ -67,17 +69,28 @@ function addItemToCart(title, price) {
             return
         }
     }
+    if(keel == "Inglise"){var cartRowContents = `
+                                  <div class="cart-item cart-column">
 
-    var cartRowContents = `
-        <div class="cart-item cart-column">
-           
-            <span class="cart-item-title">${title}</span>
-        </div>
-        <span class="cart-price cart-column">${price}</span>
-        <div class="cart-quantity cart-column">
-            <input class="cart-quantity-input" type="number" value="1">
-            <button class="btn btn-danger" type="button">REMOVE</button>
-        </div>`
+                                      <span class="cart-item-title">${title}</span>
+                                  </div>
+                                  <span class="cart-price cart-column">${price}</span>
+                                  <div class="cart-quantity cart-column">
+                                      <input class="cart-quantity-input" type="number" value="1">
+                                      <button class="btn btn-danger" type="button">REMOVE</button>
+                                  </div>`}
+                                  else {
+                                  var cartRowContents = `
+                                          <div class="cart-item cart-column">
+
+                                              <span class="cart-item-title">${title}</span>
+                                          </div>
+                                          <span class="cart-price cart-column">${price}</span>
+                                          <div class="cart-quantity cart-column">
+                                              <input class="cart-quantity-input" type="number" value="1">
+                                              <button class="btn btn-danger" type="button">EEMALDA</button>
+                                          </div>`}
+
     cartRow.innerHTML = cartRowContents
     cartItems.append(cartRow)
     updateCartTotal()
